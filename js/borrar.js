1537804a8871f508065860e37971b7eb64eead52
Manuel Agapito Samaniego Lao <80022993+Mansama18-code@users.noreply.github.com>
2024-07-04 21:55:37 -0300

    Add files via upload

---
+++ js/borrar.js
@@ -0,0 +1,63 @@
+//const URL = "http://127.0.0.1:5000/"
+const URL = "https://mansama18.pythonanywhere.com/"
+// Obtiene el contenido del inventario
+function obtenerProductos() {
+fetch(URL + 'mueble') // Realiza una solicitud GET al servidor y obtener la lista de productos.
+.then(response => {
+// Si es exitosa (response.ok), convierte los datos de la respuesta de formato JSON a un objeto JavaScript.
+
+if (response.ok) { return response.json(); }
+})
+// Asigna los datos de los productos obtenidos a lapropiedad productos del estado.
+.then(data => {
+const productosTable = document.getElementById('productos-table').getElementsByTagName('tbody')[0];
+
+productosTable.innerHTML = ''; // Limpia la tabla antes de insertar nuevos datos
+
+data.forEach(muebles => {
+const row = productosTable.insertRow();
+row.innerHTML = `
+<td>${muebles.cod_articulo}</td>
+<td>${muebles.nombre}</td>
+<td>${muebles.descripcion}</td>
+<td>${muebles.material}</td>
+<td>${muebles.cantidad}</td>
+<td >${muebles.preciomin}</td>
+<td >${muebles.preciomay}</td>
+<td><button onclick="eliminarProducto('${muebles.cod_articulo}')">Eliminar</button></td>
+`;
+});
+})
+// Captura y maneja errores, mostrando una alerta en caso de error al obtener los productos.
+.catch(error => {
+console.log('Error:', error);
+alert('Error al obtener los productos.');
+});
+}
+// Se utiliza para eliminar un producto.
+function eliminarProducto(codigo) {
+// Se muestra un diálogo de confirmación. Si el usuario confirma, 
+//se realiza una solicitud DELETE al servidor a través de fetch(URL + 'productos/${codigo}', {method: 'DELETE' }).
+
+if (confirm('¿Estás seguro de que quieres eliminar este producto?')) {
+
+fetch(URL + 'mueble/' + codigo , { method: 'DELETE' })
+.then(response => {
+if (response.ok) {
+    console.log(response);
+// Si es exitosa (response.ok), elimina el producto y da mensaje de ok.
+
+obtenerProductos(); // Vuelve a obtener la lista de productos para actualizar la tabla.
+
+alert('Producto eliminado correctamente.');
+}
+})
+// En caso de error, mostramos una alerta con un mensaje de error.
+
+.catch(error => {
+alert(error.message);
+});
+}
+}
+// Cuando la página se carga, llama a obtenerProductos para cargar la lista de productos.
+document.addEventListener('DOMContentLoaded', obtenerProductos);
---
+++ js/listado.js
@@ -0,0 +1,44 @@
+//const URL = "http://127.0.0.1:5000/";
+// Al subir al servidor, deberá utilizarse la siguiente ruta.
+//USUARIO debe ser reemplazado por el nombre de usuario de Pythonanywhere
+const URL = "https://mansama18.pythonanywhere.com/"
+
+// Realizamos la solicitud GET al servidor para obtener todos los productos.
+console.log(URL);
+fetch(URL + 'mueble').then(function (response) {
+    console.log(response);
+    if (response.ok) {
+       
+    //Si la respuesta es exitosa (response.ok), convierteel cuerpo de la respuesta de formato JSON a un objeto JavaScript y pasa estos datos a la siguiente promesa then.
+        return response.json();
+    } else {
+    // Si hubo un error, lanzar explícitamente una excepción para ser "catcheada" más adelante
+
+        throw new Error('Error al obtener los productos.');
+    }
+    }).then(function (data) { //Esta función maneja los datos convertidos del JSON. 
+    console.log(data);
+    let tablaProductos = document.getElementById('tablaProductos'); //Selecciona el elemento del DOM donde se mostrarán los productos.
+        console.log(tablaProductos);
+// Iteramos sobre cada producto y agregamos filas a la tabla
+
+    for (let muebles of data) {
+    let fila = document.createElement('tr'); //Crea una nueva fila de tabla (<tr>) para cada producto.
+
+    fila.innerHTML = '<td>' + muebles.cod_articulo + '</td>' + '<td >' + muebles.nombre + '</td>'  + '<td>' +
+    muebles.descripcion + '</td>' + '<td>' + muebles.material + '</td>' + 
+    '<td>' + muebles.cantidad + '</td>' + '<td >' + muebles.preciomin + '</td>' +
+    '<td>' + muebles.preciomay + '</td>';
+
+    //Una vez que se crea la fila con el contenido del
+    //producto, se agrega a la tabla utilizando el método appendChild del
+    //elemento tablaProductos.
+    
+            tablaProductos.appendChild(fila);
+            }
+            })
+    //Captura y maneja errores, mostrando una alerta en caso de error al obtener los productos.
+    .catch(function (error) {
+    // Código para manejar errores
+    alert('Error al obtener los productos.');
+    });
